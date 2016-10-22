0fed750691e01b1eb66395c0257f6a26d6255689
Lubomir Host <lubomir.host@gmail.com>
2016-10-22 23:42:48 +0200

    Don't remove build tools'

--- definitions/Debian-jessie-amd64-netboot/cleanup.sh
+++ definitions/Debian-jessie-amd64-netboot/cleanup.sh
@@ -1,5 +1,5 @@
 # Clean up
-apt-get -y remove linux-headers-$(uname -r) build-essential
+#apt-get -y remove linux-headers-$(uname -r) build-essential
 apt-get -y autoremove
 apt-get -y clean
 
@@ -9,8 +9,7 @@ rm /var/lib/dhcp/*
 
 # Make sure Udev doesn't block our network
 echo "cleaning up udev rules"
-rm /etc/udev/rules.d/70-persistent-net.rules
-mkdir /etc/udev/rules.d/70-persistent-net.rules
+echo -n > /etc/udev/rules.d/70-persistent-net.rules
 rm -rf /dev/.udev/
 rm /lib/udev/rules.d/75-persistent-net-generator.rules
 
